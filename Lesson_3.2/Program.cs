f8ddf19eaa6db9a255d38af37cbcdb844b62d5c9
arman.p <arman.p@F4RPDEV-62>
2022-08-03 11:08:31 +0400

    Created objects for classes

--- Lesson_3.2/Program.cs
+++ Lesson_3.2/Program.cs
@@ -6,7 +6,12 @@ namespace Lesson_3._2
     {
         static void Main(string[] args)
         {
-      
+            Chipboard chipboard = new Chipboard("Chipboard", "dark", "Orange", 1000);
+            Plywood plywood = new Plywood("Plywood", "light", "Cream", 3000);
+            Beech beech = new Beech("Beech", "light", "Chockolate", 5000);
+            chipboard.PrintAboutItem();
+            plywood.PrintAboutItem();
+            beech.PrintAboutItem();
         }
     }
 }
