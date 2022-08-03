1cf921141c43e03051330b90933f417987f82c7b
arman.p <arman.p@F4RPDEV-62>
2022-08-03 10:28:33 +0400

    Add project files.

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_3
+{
+    class Person
+    {
+        protected string name;
+        protected int age;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+        }
+
+    }
+ 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
