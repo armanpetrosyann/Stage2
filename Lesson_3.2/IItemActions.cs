e59c1898f8d804d0d8039214680204a2c649e9dd
arman.p <arman.p@F4RPDEV-62>
2022-08-03 10:49:01 +0400

    Added new exercise

---
+++ Lesson_3.2/IItemActions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_3._2
+{
+    internal interface IItemActions
+    {
+
+    }
+}
---
+++ Lesson_3.2/Wood.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_3._2
+{
+    internal class Wood
+    {
+        protected string name;
+        protected string type;
+        protected string color;
+        protected decimal price;
+
+        public Wood(string name, string type, string color, decimal price)
+        {
+            this.name = name;
+            this.type = type;
+            this.color = color;
+            this.price = price;
+        }
+    }
+}
